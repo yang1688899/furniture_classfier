34d5798061ad502231f24ae554797ad1621bff0f
yang <yang535460548484@gmail.com>
2018-04-23 02:55:07 +0800

    commit

--- data.py
+++ data.py
@@ -2,26 +2,48 @@ import json
 import cv2
 import os
 import numpy as np
+from sklearn.utils import shuffle
+from sklearn.preprocessing import LabelBinarizer
+from keras.applications.vgg19 import preprocess_input
+from keras.preprocessing import image
 
-file = open('G:/fourniture_classification/validation.json')
-validation = json.load(file)
+# file = open('f:/fourniture_classification/validation.json')
+# train = json.load(file)
+#
+# print(len(train['annotations']))
 
-print(validation['annotations'])
-
-def data_gen(dir,labelfile_path,batch_size=32):
+def data_gen(dir,labelfile_path,batch_size=32,is_shuffle=True):
     labelfile = open(labelfile_path)
     annotation_list = json.load(labelfile)['annotations']
-    for offset in range(0,len(annotation_list),batch_size):
-        features = []
-        labels = []
-        for i in range(offset,offset+batch_size):
-            img_path = '%s/%s.jpg'%(dir,annotation_list[i]["image_id"])
-            if os.path.exists(img_path):
-                img = cv2.imread(img_path)
-                img = cv2.resize(img,(224,224))
-                features.append(img)
-                labels.append(annotation_list[i]["label_id"])
-        yield np.array(features),np.array(labels)
+    num_sample = len(annotation_list)
+    image_ids = [annotation_list[i]["image_id"] for i in range(num_sample)]
+    image_labels = [annotation_list[i]['label_id'] for i in range(num_sample)]
+    image_labels = LabelBinarizer().fit_transform(image_labels)
+    while True:
+
+        if is_shuffle:
+            image_ids,image_labels = shuffle(image_ids, image_labels)
+
+        for offset in range(0,num_sample,batch_size):
+
+            if is_shuffle:
+                image_ids,image_labels = shuffle(image_ids,image_labels)
+
+            features = []
+            labels = []
+            batch_ids = image_ids[offset:offset+batch_size]
+            batch_labels = image_labels[offset:offset+batch_size]
+            for i in range(len(batch_ids)):
+                img_path = '%s/%s.jpg'%(dir,batch_ids[i])
+                if os.path.exists(img_path):
+                    img = image.load_img(img_path, target_size=(224, 224))
+                    img = image.img_to_array(img)
+                    img = preprocess_input(img)
+                    # img = cv2.imread(img_path)
+                    # img = cv2.resize(img,(224,224))
+                    features.append(preprocess_input(img))
+                    labels.append(batch_labels[i])
+            yield np.array(features), np.array(labels)
 
 
 
--- model.py
+++ model.py
@@ -2,34 +2,48 @@ from keras.applications.vgg19 import VGG19
 from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
 from keras.models import Model
-from keras.layers import Dense, Flatten
+from keras.layers import Dense, Flatten,GlobalAveragePooling2D
 import numpy as np
 import data
+from math import ceil
+from keras.models import load_model
 
-# img = image.load_img('G:/fourniture_classification/validation/1.jpg', target_size=(224, 224))
-# x = image.img_to_array(img)
-# x = np.expand_dims(x, axis=0)
-# x = preprocess_input(x)
+num_train = 194828
+num_validation = 6400
+
 
 def network():
-    base_model = VGG19(weights='imagenet', include_top=False)
+    base_model = VGG19(weights='imagenet', include_top=False,input_shape=(224,224,3))
     x = base_model.output
     x = Flatten()(x)
+    # x = GlobalAveragePooling2D()(x)
     x = Dense(4096, activation='relu')(x)
     x = Dense(4096, activation='relu')(x)
-    predictions = Dense(80, activation='softmax')(x)
+    predictions = Dense(128, activation='softmax')(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     return  base_model,model
 
-def train():
-    gen = data.data_gen('G:/fourniture_classification/validation', 'G:/fourniture_classification/validation.json')
-    base_model,model = network()
-    model.summary()
-    # for layer in base_model.layers:
-    #     layer.trainable = False
-    #
-    # model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    #
-    # model.model.fit_generator()
+# def get_accuracy(prediction):
+#     np.max
 
-train()
+def train(batch_size=32):
+    # train_gen = data.data_gen('G:/fourniture_classification/validation', 'G:/fourniture_classification/validation.json',batch_size=batch_size)
+    # valid_gen = data.data_gen('G:/fourniture_classification/validation', 'G:/fourniture_classification/validation.json',batch_size=batch_size,is_shuffle=False)
+    train_gen = data.data_gen('f:/fourniture_classification/validation', 'f:/fourniture_classification/validation.json',
+                              batch_size=batch_size)
+    valid_gen = data.data_gen('f:/fourniture_classification/validation', 'f:/fourniture_classification/validation.json',
+                              batch_size=batch_size, is_shuffle=False)
+    base_model,model = network()
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model.compile(optimizer='Adam', loss='categorical_crossentropy')
+
+    model.fit_generator(train_gen, steps_per_epoch=ceil(num_train/batch_size), epochs=1,validation_data=valid_gen, validation_steps=ceil(num_validation/batch_size))
+
+    model.save('./model.h5')
+
+# train(batch_size=24)
+
+model = load_model('./model.h5')
+model.summary()
